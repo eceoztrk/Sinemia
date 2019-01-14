adf5fff4907287894e372d3ce1770a716d7b7459
EceOzturk <ece@eceozturk-macbook-pro.local>
2019-01-14 12:15:59 -0800

    Initial Commit

---
+++ Sinemia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sinemia
+//
+//  Created by EceOzturk on 14.01.2019.
+//  Copyright © 2019 EceOzturk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
